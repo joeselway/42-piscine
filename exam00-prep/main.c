98ad4285c37a4d6f43fdf3dfcab153a887a0cd9f
Joe Selway <jselway@student.42adel.org.au>
2021-11-26 13:35:28 +1030

    exams tuff

--- exam00-prep/main.c
+++ exam00-prep/main.c
@@ -1,6 +1,23 @@
 #include <unistd.h>
+#include <stdio.h>
 
-int	main(int argv, char **argv)
+int	main(int argc, char** argv)
 {
-	write(1, &argv[1], 1);
-}
+	int i;
+	i = 0;
+	if (argc == 2)
+	{
+		while argv[1][i] != '\0'
+		{
+			if (av[1][i]
+	}
+
+/*
+	while(*argv)
+	{
+		printf("the printf output is: %s\n", *argv);
+		write(1, *argv[0], strlen(*argv);
+		argv++;
+	}
+ 
+*/
