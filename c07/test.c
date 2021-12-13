34410afbd8db22cf382b8cfbcd31b564d7006fb8
Joe Selway <17176434+joeselway@users.noreply.github.com>
2021-12-13 20:43:09 +1030

    testing

--- c07/test.c
+++ c07/test.c
@@ -2,8 +2,16 @@
 
 int	main(void)
 {
+	int	i;
+	int	*ip;
+	long *l;
+	char *a;
 	char c = 'a';
 	char *p = &c;
-	printf("%lu\n", sizeof(p));
-	printf("%lu\n", sizeof(*p));
+	printf("Dereferenced pointer to char is size: %lu\n", sizeof(*p));
+	printf("Pointer to char is size: %lu\n", sizeof(p));
+	printf("Dereferenced uninitialised pointer to char is size: %lu\n", sizeof(*a));
+	printf("Dereferenced uninitialised pointer to int is size: %lu\n", sizeof(*ip));
+	printf("Dereferenced uninitialised pointer to long is size: %lu\n", sizeof(*l));
+	printf("Integer is size: %lu\n", sizeof(i));
 }
