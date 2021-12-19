896335043dc8b3a81261e8ce43087b32e5a8566f
Joe Selway <jselway@student.42adel.org.au>
2021-12-19 16:15:03 +1030

    c08

--- c08/ex04/ft_strs_to_tab.c
+++ c08/ex04/ft_strs_to_tab.c
@@ -6,14 +6,72 @@
 /*   By: jselway <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 20:48:11 by jselway           #+#    #+#             */
-/*   Updated: 2021/12/18 16:43:16 by jselway          ###   ########.fr       */
+/*   Updated: 2021/12/19 15:35:02 by jselway          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "ft_stock_str.h"
+#include <stdlib.h>
 
-
-
-int	main(void)
+int	ft_strlen(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int					i;
+	struct s_stock_str	*result;
+
+	result = malloc(sizeof(*result) * ac + 1);
+	i = 0;
+	while (i < ac)
+	{
+		result[i].size = ft_strlen(av[i]);
+		result[i].str = malloc(sizeof(av[i][0]) * (result[i].size + 1));
+		result[i].copy = malloc(sizeof(av[i][0]) * (result[i].size + 1));
+		ft_strcpy(result[i].str, av[i]);
+		ft_strcpy(result[i].copy, av[i]);
+		i++;
+	}
+	result[i].str = 0;
+	result[i].size = 0;
+	result[i].size = 0;
+	return (result);
+}
+/*
+int	main(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	struct s_stock_str *struct_array;
+	struct_array = ft_strs_to_tab(ac, av);
+	while (i < ac)
+	{
+		ft_show_tab(&struct_array[i]);
+		i++;
+	}
+}
+*/
--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -6,11 +6,14 @@
 /*   By: jselway <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:56:08 by jselway           #+#    #+#             */
-/*   Updated: 2021/12/18 16:43:20 by jselway          ###   ########.fr       */
+/*   Updated: 2021/12/19 15:33:25 by jselway          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putstr(*str)
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putstr(char *str)
 {
 	while (*str)
 	{
@@ -22,12 +25,31 @@ void	ft_putstr(*str)
 
 void	ft_putnbr(int nb)
 {
-		
+	long int	nbl;
+	int			div;
+	int			mod;
+	char		modchar;
+
+	nbl = nb;
+	if (nbl < 0)
+	{
+		nbl = nbl * -1;
+		write(1, "-", 1);
+	}
+	div = nbl / 10;
+	mod = nbl % 10;
+	if (div > 0)
+	{
+		ft_putnbr(div);
+	}
+	modchar = mod + 48;
+	write(1, &modchar, 1);
 }
 
 void	ft_show_tab(struct s_stock_str *par)
 {
-	ft_putstr(par.str);
-	ft_putnbr(par.size);
-	ft_putstr(par.copy);
+	ft_putstr(par->str);
+	ft_putnbr(par->size);
+	write(1, "\n", 1);
+	ft_putstr(par->copy);
 }
